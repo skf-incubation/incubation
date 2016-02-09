6791676f101afe37737c874132d58fbc09060eed
Rahul Sharma <rsconceptx@gmail.com>
2016-02-09 20:52:27 +0530

    adding schemas to users

---
+++ client/events/admin.js
@@ -0,0 +1,18 @@
+Template.manage_events.events({
+	'click .add-event': function() {
+		Session.set('manage-event-mode',true);
+	}
+});
+
+Template.manage_events.helpers({
+	'admin_manage_mode': function () {
+		return Sessiong.get('manage-event-mode');
+	}
+});
+
+Template.add_event_form.events({
+	'submit .add-event-btn': function(event) {
+		event.preventDefault();
+		Session.set('manage-event-mode',!(Session.get('manage-event-mode')));
+	}
+})
