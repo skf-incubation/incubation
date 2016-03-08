14ce52c5ba750e13a32ed7315a1380fc40c8df1c
Rahul Sharma <rsconceptx@gmail.com>
2016-03-08 21:03:48 +0530

    changes to events page

---
+++ client/helpers/events.js
@@ -0,0 +1,21 @@
+Template.events_grid.helpers({
+	'events': function() {
+		return [
+	{
+		'event_name':"Robotics",
+		'event_date': new Date(),
+		'event_desc':"A manual robotics workshop teaching students of 1st year and 2nd year to dive into the exciting field of animating machines for fun and learning",
+	},
+	{
+		'event_name':"Coding",
+		'event_date': new Date(),
+		'event_desc':"kfslkfjlkse",
+	},
+	{
+		'event_name':"Gaming",
+		'event_date': new Date(),
+		'event_desc':"kfslkfjlkse",
+	},
+	]
+	} 
+});
